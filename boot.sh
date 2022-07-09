072eec829f99dd84a8e952c507566bbfc950aa9c
Andy <lecoqandy@gmail.com>
2022-07-09 15:37:33 +0200

    Initial vagrantfile

---
+++ boot.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This script install Docker without Containerd. So it s a Full Docker
+sudo apt-get update
+sudo apt-get install -yqq apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -yqq docker-ce docker-ce-cli
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo apt-get install git
